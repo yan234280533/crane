a7b1c527ebcef950e7e566ea8f0f9e964fc8c11a
devinyan <devinyan@tencent.com>
2021-11-27 15:30:30 +0800

    feat: add node local cpu collect

--- pkg/known/label.go
+++ pkg/known/label.go
@@ -4,3 +4,8 @@ const (
 	AdvancedHorizontalPodAutoscalerUidLabel  = "autoscaling.crane.io/advanced-hpa-uid"
 	AdvancedHorizontalPodAutoscalerManagedBy = "advanced-hpa-controller"
 )
+
+const (
+	EnsuranceAnalyzedPressureTaintKey     = "ensurance.crane.io/analyzed-pressure"
+	EnsuranceAnalyzedPressureConditionKey = "analyzed-pressure"
+)
