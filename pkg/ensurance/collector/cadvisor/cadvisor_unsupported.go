1d53fc3a8d2d0004c0038acd3f331da6e8fc1177
devinyan <devinyan@tencent.com>
2022-03-17 23:40:37 +0800

    test for manager

--- pkg/ensurance/collector/cadvisor/cadvisor_unsupported.go
+++ pkg/ensurance/collector/cadvisor/cadvisor_unsupported.go
@@ -6,10 +6,16 @@ package cadvisor
 import (
 	"github.com/gocrane/crane/pkg/common"
 	"github.com/gocrane/crane/pkg/ensurance/collector/types"
+	info "github.com/google/cadvisor/info/v1"
+	cadvisorapiv2 "github.com/google/cadvisor/info/v2"
 	corelisters "k8s.io/client-go/listers/core/v1"
 )
 
 type CadvisorCollector struct {
+	Manager Manager
+}
+
+type Manager struct {
 }
 
 func NewCadvisor(_ corelisters.PodLister) *CadvisorCollector {
@@ -30,4 +36,12 @@ func (c *CadvisorCollector) Collect() (map[string][]common.TimeSeries, error) {
 
 func CheckMetricNameExist(name string) bool {
 	return false
+}
+
+func (m *Manager) ContainerInfoV2(containerName string, options cadvisorapiv2.RequestOptions) (map[string]cadvisorapiv2.ContainerInfo, error) {
+	return nil, nil
+}
+
+func (m *Manager) GetContainerInfo(containerName string, query *info.ContainerInfoRequest) (*info.ContainerInfo, error) {
+	return nil, nil
 }
